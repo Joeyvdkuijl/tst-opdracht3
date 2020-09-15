1fb5f9bed36d479eaba662b850375de996a59abc
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-09-15 10:37:05 +0200

    Add files via upload

--- opdracht3/info.js
+++ opdracht3/info.js
@@ -7,3 +7,5 @@ function Student(id, klas) {
   let student4 =  newStudent('32900','Md1b');
   let student5 =  newStudent('42011','Md1a');
   let student6 =  newStudent('66400','Md1a');
+
+  localStorage,setItem('student1','student2','student3','student4','student5','student6');
