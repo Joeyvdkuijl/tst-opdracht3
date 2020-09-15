9b5471ef8abef869692a87ab6cab75c383dab7e8
Joeyvdkuijl <55228974+Joeyvdkuijl@users.noreply.github.com>
2020-09-15 10:28:14 +0200

    Add files via upload

---
+++ opdracht3/info.js
@@ -0,0 +1,9 @@
+function Student(id, klas) {
+    studentNummer.id = id;
+    klasNaam.klas = klas;
+  }
+  let student1 =  newStudent('30226','Md1a');
+  let student2 =  newStudent('45778','Md1b');
+  let student4 =  newStudent('32900','Md1b');
+  let student5 =  newStudent('42011','Md1a');
+  let student6 =  newStudent('66400','Md1a');
